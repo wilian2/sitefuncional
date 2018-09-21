ac62983f6804f8c4739fd083c62282a07404de90
wilian2 <wilianlopesdesouza@gmail.com>
2018-09-21 09:05:00 -0300

    Update conexaobd.php

--- conexaobd.php
+++ conexaobd.php
@@ -4,7 +4,8 @@ $host = getenv("OPENSHIFT_MYSQL_DB_HOST");
 $username = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
 $port = getenv("OPENSHIFT_MYSQL_DB_PORT");
 $password = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-$dbname = "tesouro";
+
+print_r($host);
 
 
 ?>
