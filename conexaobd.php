eee86e23f9c3f34e47d00915ad381d64e98cd9cf
wilian2 <wilianlopesdesouza@gmail.com>
2018-09-21 09:51:56 -0300

    Update conexaobd.php

--- conexaobd.php
+++ conexaobd.php
@@ -4,11 +4,14 @@ $banco = "tesourodb";
 $usuario = "userXFF";
 $senha = "HaVXqmhqeYkqETAB";
 $hostname = "127.0.0.1";
-$conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die( "Não foi possível conectar ao banco MySQL");
-if (!$conn) {echo "Não foi possível conectar ao banco MySQL.
-"; exit;}
-else {echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.
-";}
+$conn = mysql_connect($hostname,$usuario,$senha);mysql_select_db($banco) or die( "Não foi possível conectar ao banco MySQL");
+if (!$conn) {
+  echo "Não foi possível conectar ao banco MySQL.";
+  exit;
+}
+else {
+  echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.";
+}
 mysql_close(); 
 ?>
 
