4d5ac34b89eae9754fcc6073dba3dea06dafa8af
wilian2 <wilianlopesdesouza@gmail.com>
2018-09-21 09:32:17 -0300

    Update conexaobd.php

--- conexaobd.php
+++ conexaobd.php
@@ -1,11 +1,14 @@
 <?php
 // Conecta-se com o MySQL
-$host = getenv("OPENSHIFT_MYSQL_DB_HOST");
-$username = getenv("OPENSHIFT_MYSQL_DB_USERNAME");
-$port = getenv("OPENSHIFT_MYSQL_DB_PORT");
-$password = getenv("OPENSHIFT_MYSQL_DB_PASSWORD");
-
-print_r($host);
-
-
+$banco = "tesourodb";
+$usuario = "userXFF";
+$senha = "HaVXqmhqeYkqETAB";
+$hostname = "127.0.0.1";
+$conn = mysql_connect($hostname,$usuario,$senha); mysql_select_db($banco) or die( "Não foi possível conectar ao banco MySQL");
+if (!$conn) {echo "Não foi possível conectar ao banco MySQL.
+"; exit;}
+else {echo "Parabéns!! A conexão ao banco de dados ocorreu normalmente!.
+";}
+mysql_close(); 
 ?>
+
